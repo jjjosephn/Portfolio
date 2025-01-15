b70d0288daa36098261a87523bd0ee6075e45329
Joseph Nguyen <joseph.h.nguyen@sjsu.edu>
2025-01-14 19:02:58 -0800

    Implement a complete navbar

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { BrowserRouter } from 'react-router-dom'
+import About from './components/About'
+import Navbar from './components/Navbar'
+import Hero from './components/Hero'
+import Experience from './components/Experience'
+import Works from './components/Works'
+import Feedbacks from './components/Feedbacks'
+import Tech from './components/Tech'
+import Contact from './components/Contact'
+import Stars from './components/canvas/Stars'
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-patterm bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+      </div>
+      <div className='relative z-0'>
+        <Contact />
+        <Stars />
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
