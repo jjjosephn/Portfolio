865dcdf9d04c0426328cd5fe3263d268fccfdf83
Joseph Nguyen <joseph.h.nguyen@sjsu.edu>
2025-01-15 18:28:34 -0800

    Fix bugs in the hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
   return (
     <section className='relative w-full h-screen mx-auto'>
       <div className={`${styles.paddingX}
-        absolute inset-0 top-[120px] max-w-7xl mx auto flex flex-row items-start gap-5`}
+        absolute inset=0 z-10 top-[120px] max-w-7xl mx auto flex flex-row items-start gap-5`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-[#915eff]' />
@@ -20,7 +20,7 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915eff]'>Joseph</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I'm a senior at &nbsp;
+            A senior at &nbsp;
             <a 
               href='https://www.sjsu.edu/cs/'
               className='text-[#005fb5] hover:underline'
@@ -35,7 +35,7 @@ const Hero = () => {
       
       <ComputersCanvas />
 
-      <div className='absolute xs:bottom-10 bottom;32 w-full flex justify-center items-center'>
+      <div className='absolute bottom-1 w-full flex justify-center items-center'>
         <a href='#about'>
           <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
             <motion.div 
