97e2e92029529f0e123e1b6a7f54f93995e49193
Joseph Nguyen <joseph.h.nguyen@sjsu.edu>
2025-01-20 18:05:44 -0800

    Resolve desktop build error

--- src/components/canvas/index.js
+++ src/components/canvas/index.js
@@ -2,6 +2,6 @@ import EarthCanvas from "./Earth";
 import BallCanvas from "./Ball";
 import ComputersCanvas from "./Computers";
 import StarsCanvas from "./Stars";
-import DesktopCanvas from "./desktop";
+import DesktopCanvas from "./Desktop";
 
 export { EarthCanvas, BallCanvas, ComputersCanvas, StarsCanvas, DesktopCanvas };
